fbedcbe651cf28b0c670c0a5f3551aced80de2e4
pauldambra <pauldambra@gmail.com>
2016-12-05 22:41:30 +0000

    add day 5 solutions

--- day_5/spec/part_one.rb
+++ day_5/spec/part_one.rb
@@ -1,23 +1,57 @@
 
 # --- Day 5: How About a Nice Game of Chess? ---
 
-# You are faced with a security door designed by Easter Bunny engineers that seem to have acquired most of their security knowledge by watching hacking movies.
+# You are faced with a security door designed by Easter Bunny engineers 
+# that seem to have acquired most of their security knowledge by watching hacking movies.
 
-# The eight-character password for the door is generated one character at a time by finding the MD5 hash of some Door ID (your puzzle input) and an increasing integer index (starting with 0).
+# The eight-character password for the door is generated one character at a time 
+# by finding the MD5 hash of some Door ID (your puzzle input) and an increasing integer index 
+# (starting with 0).
 
-# A hash indicates the next character in the password if its hexadecimal representation starts with five zeroes. If it does, the sixth character in the hash is the next character of the password.
+# A hash indicates the next character in the password if its hexadecimal representation
+# starts with five zeroes. If it does, the sixth character in the hash is the next character of the password.
 
 # For example, if the Door ID is abc:
 
-# The first index which produces a hash that starts with five zeroes is 3231929, which we find by hashing abc3231929; the sixth character of the hash, and thus the first character of the password, is 1.
-# 5017308 produces the next interesting hash, which starts with 000008f82..., so the second character of the password is 8.
-# The third time a hash starts with five zeroes is for abc5278568, discovering the character f.
-# In this example, after continuing this search a total of eight times, the password is 18f47a30.
+# The first index which produces a hash that starts with five zeroes is 3231929, 
+# which we find by hashing abc3231929; the sixth character of the hash, and thus the
+#  first character of the password, is 1.
+
+# 5017308 produces the next interesting hash, which starts with 000008f82...,
+# so the second character of the password is 8.
+
+# The third time a hash starts with five zeroes is for abc5278568,
+# discovering the character f.
+
+# In this example, after continuing this search a total of eight times,
+#  the password is 18f47a30.
 
 # Given the actual Door ID, what is the password?
 
 # Your puzzle input is ojvtpuvg.
 
-describe "day 5 - part one - how about a nice game of chess" do
+module DoorHacker
+  module_function
 
+  def hack(door_id)
+    password_chars = []
+    index = 0
+    while password_chars.length < 8 do
+      hash = Digest::MD5.hexdigest "#{door_id}#{index}"
+      password_chars.push(hash[5]) if hash.start_with?('00000') 
+      index += 1
+    end
+
+    return password_chars.join('')
+  end
+end
+
+describe "day 5 - part one - how about a nice game of chess" do
+  it "can find the example password" do
+    expect(DoorHacker.hack 'abc').to eq('18f47a30')
+  end
+
+  it "solve the problem" do
+    p "puzzle solution is #{DoorHacker.hack 'ojvtpuvg'}"
+  end
 end
